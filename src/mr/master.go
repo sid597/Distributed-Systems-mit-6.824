2df7a0e86f9249bacf5b9f993fc879e82528f45b
sid597 <siddharthdv77@gmail.com>
2020-09-29 01:45:42 +0530

    1. Added declarations for Master, Workers, Files 2. Trying to add new way of replying to map worker 3. Initialize new Job, Worker#

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,4 +1,4 @@
-git clone git://g.csail.mit.edu/6.824-golabs-2020 6.824package mr
+package mr
 
 import "log"
 import "net"
@@ -8,10 +8,44 @@ import "net/http"
 import "fmt"
 
 
-type Master struct {
+////////////////////////////////////////////////
+// Declarations
+////////////////////////////////////////////////
+
+type MasterDetails struct {
 	// Your definitions here.
+    AllWorkers []WorkerDetails
+    UnfinishedTasks []string
 
 }
+
+type WorkerDetails struct {
+    Id int
+    Task string
+    Filename string
+    Status string
+}
+
+// The operations related to files should be global
+// so that one can access these without initializing or passing some other data to functions
+var (
+    Job Files
+    Master MasterDetails
+    WorkerCtr int
+    NoNewFile string = "No New FIle234"
+)
+
+type Files struct {
+    AllFiles []string
+    FilesLen, CurrentFileCtr int
+    CurrentFile string
+}
+
+
+
+////////////////////////////////////////////////
+// RPC Handlers
+////////////////////////////////////////////////
 
 // Your code here -- RPC handlers for the worker to call.
 
@@ -20,59 +54,119 @@ type Master struct {
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
+func (m *MasterDetails) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-func (m *Master) GetFilename(noArg *NoArgs, fn *Files) error {
-    fmt.Println(FileNames)
-    fn.Filename = FileNames[0]
+func (m *MasterDetails) GetFilename(noArg *NoArgs, fn *MapFile) error {
+    nw := NewWorkerDetails(Job.CurrentFile)
+    fmt.Println(nw)
+    fn.Name = nw.Filename
+//    if Job.CurrentFileCtr != Job.AllFilesLen - 1{
+//        IncreaseCurrentJobFileCtr()
+//    } else {
+//        Job.CurrentFile = NoNewFile
+//    }
     return nil
 }
 
-//
-// start a thread that listens for RPCs from worker.go
-//
-func (m *Master) server() {
-	rpc.Register(m)
-	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", ":1234")
-//	sockname := masterSock()
-//	os.Remove(sockname)
-//	l, e := net.Listen("unix", sockname)
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	go http.Serve(l, nil)
-}
-
-//
-// main/mrmaster.go calls Done() periodically to find out
-// if the entire job has finished.
-//
-func (m *Master) Done() bool {
-	ret := false
-
-	// Your code here.
 
 
-	return ret
-}
+////////////////////////////////////////////////
+// Master server 
+////////////////////////////////////////////////
+
 
 //
 // create a Master.
 // main/mrmaster.go calls this function.
 // nReduce is the number of reduce tasks to use.
 //
-func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
+func MakeMaster(files []string, nReduce int) *MasterDetails {
+	Master = MasterDetails{}
 
 	// Your code here.
+    NewJob(files)
+    fmt.Println(Job.AllFiles)
 
-    FileNames = files
-    fmt.Println(FileNames)
-
-	m.server()
-	return &m
+	Master.server()
+	return &Master
 }
+
+//
+// start a thread that listens for RPCs from worker.go
+//
+func (m *MasterDetails) server() {
+    rpc.Register(m)
+    rpc.HandleHTTP()
+    l, e := net.Listen("tcp", ":1234")
+//	sockname := masterSock()
+//	os.Remove(sockname)
+//	l, e := net.Listen("unix", sockname)
+    if e != nil {
+        log.Fatal("listen error:", e)
+    }
+    go http.Serve(l, nil)
+}
+
+//
+// main/mrmaster.go calls Done() periodically to find out
+// if the entire job has finished.
+//
+func (m *MasterDetails) Done() bool {
+    ret := false
+
+    // Your code here.
+
+
+    return ret
+}
+
+////////////////////////////////////////////////
+// Helper functions  
+/////////////////////////////////////////////////
+
+// New Files struct invocation
+func NewJob(files []string) {
+    Job = Files{}
+    Job.AllFiles = files
+    Job.FilesLen = len(files)
+    Job.CurrentFileCtr = 0
+    Job.CurrentFile = files[0]
+    //return f
+}
+
+func NewWorkerDetails(filename string) WorkerDetails {
+    w := WorkerDetails{}
+    w.Id = WorkerCtr
+    w.Filename = filename
+    w.Status = "in-progress"
+    return w
+}
+
+
+
+func AddToWorkersList(worker WorkerDetails) {
+    Master.AllWorkers = append(Master.AllWorkers, worker)
+}
+
+func UpdateJobData(ctr int) bool {
+    if Job.CurrentFileCtr == Job.FilesLen {
+        return false
+    } else {
+        Job.CurrentFileCtr = ctr
+        Job.CurrentFile = Job.AllFiles[Job.CurrentFileCtr]
+    }
+    return true
+}
+
+func GetJobFile(ctr int) string {
+    return Job.AllFiles[ctr]
+}
+
+func IncreaseCurrentJobFileCtr() {
+    Job.CurrentFileCtr += 1
+    Job.CurrentFile = Job.AllFiles[Job.CurrentFileCtr]
+}
+
