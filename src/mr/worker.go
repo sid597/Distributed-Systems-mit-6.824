b248fdab675a5742ed3bc00de91ea2b790f19bbc
sid597 <siddharthdv77@gmail.com>
2020-09-30 03:11:42 +0530

    90% done

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,20 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+    "fmt"
+    "log"
+    "net/rpc"
+    "hash/fnv"
+    "os"
+    "io/ioutil"
+    "time"
+)
 
+
+
+////////////////////////////////////////////////
+// Declarations
+////////////////////////////////////////////////
 
 //
 // Map functions return a slice of KeyValue.
@@ -14,6 +24,85 @@ type KeyValue struct {
 	Value string
 }
 
+type WorkerDetails struct {
+    Id int
+    Task string
+    MapFileName string
+    ReduceFileName string
+    Filename string
+    Pid int
+    nReduce int
+}
+
+var CurrentWorker WorkerDetails
+
+////////////////////////////////////////////////
+// RPC
+////////////////////////////////////////////////
+
+
+
+
+////////////////////////////////////////////////
+// Main Functions  
+////////////////////////////////////////////////
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) bool {
+    CurrentWorker = GetTask()
+    for CurrentWorker.Filename != "Secret key 994" {
+        fmt.Println(CurrentWorker)
+        if CurrentWorker.Task == "MapT"{
+            res := MapTask(CurrentWorker.MapFileName, mapf)
+            time.Sleep(500 * time.Millisecond)   // Used to test map running in parallel or not
+            return res
+        } else if CurrentWorker.Task == "ReduceT" {
+            res := ReduceTask(CurrentWorker.ReduceFileName, reducef)
+            time.Sleep(500 * time.Millisecond)   // Used to test reduce running in parallel or not
+            return res
+        }
+        CurrentWorker  = GetTask()
+    }
+}
+
+
+////////////////////////////////////////////////
+// Map Reduce functions 
+////////////////////////////////////////////////
+
+// Perform map job for the current file with the passed map func
+func MapJob(filename string, mapf func(string,string)[]KeyValue) bool {
+        intermediate := []KeyValue{}
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+            return false
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", filename)
+            return false
+		}
+        //fmt.Println(content)
+		file.Close()
+        //fmt.Println(string(content))
+		kva := mapf(filename, string(content))
+		intermediate = append(intermediate, kva...)
+        // TODO : instead of using intermediate here need to store in a file
+
+
+        return true
+}
+
+
+// Perform Reduce Job for the current file with the passed reduce function
+
+
+////////////////////////////////////////////////
+// Helper functions
+////////////////////////////////////////////////
 //
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
@@ -24,52 +113,28 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
-// main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the master.
-	 CallExample()
-     GetFileName()
-
-}
-
-//
-// example function to show how to make an RPC call to the master.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func GetFileName() {
+// Ask for new task from Master
+func GetTask() string{
     na := NoArgs{}
-    rply := Files{}
-    call("Master.GetFilename",&na, &rply)
-    fmt.Println("reply receives is ", rply.Filename)
-    fmt.Printf("filename from master ", rply)
-}
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	call("Master.Example", &args, &reply)
-
-	// reply.Y should be 100.
-	fmt.Printf("reply.Y %v\n", reply.Y)
+    NewTask := Task{}
+    call("MasterDetails.GetFilename",&na, &NewTask)
+    // fmt.Println("reply received is ", NewTask.Name)
+    return NewTask
 }
 
-//
+
+// Create nReduce Temporary Map files 
+func CreateFile(dir, filename string) bool  {
+    // TODO
+}
+
+// Write Map KeyValue Slice output to R-th file
+func WriteMapTo(filename string,KV []KeyValue) bool {}
+
+
+////////////////////////////////////////////////
+// Send RPC
+////////////////////////////////////////////////
 // send an RPC request to the master, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
