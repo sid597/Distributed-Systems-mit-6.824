79c528f4b25ca4b148101f1e354fb5bc1bcbfed9
sid597 <siddharthdv77@gmail.com>
2020-12-01 14:05:01 +0530

    Modfied utility function name, made it shorter Pf(print format) from DPrintf

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,9 +3,9 @@ package raft
 import "log"
 
 // Debugging
-const Debug = 0
+const Debug = 1
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
+func Pf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
 		log.Printf(format, a...)
 	}
